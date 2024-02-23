6011874029f0fba30725f5a37b0d93e7d5801557
Kristina Macias <kristinamacias@Kristinas-MBP.lan>
2024-02-23 01:30:46 -0800

    remove nav import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import TechStack from "./components/TechStack";
 import Recommendations from "./components/Recommendations";
-import Navigation from "./components/Navigation";
 import "./App.css";
 
 
