626534e489d5c2d4517b302801cf449a39868e22
Kristina Macias <kristinamacias@Kristinas-MBP.lan>
2024-03-06 11:51:55 -0800

    adjusting margins

--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -5,17 +5,19 @@ import backgroundSVG from "../assets/circles-black.svg";
 const Recommendations = () => {
   return (
     <section className="web-section">
-      <div className="paragraph-container">
-      <img className="svg-background" src={backgroundSVG} alt="" />
-      <h1>Say What..?!</h1>
-        {testimonialData.content.map((testimonial) => {
-          return (
-            <div key={testimonial.id} className="recommendation">
-              <p>"{testimonial.highlight}"</p>
-              <p className="author">{testimonial.name}</p>
-            </div>
-          );
-        })}
+      <div className="testimonials-container">
+        <img className="svg-background" src={backgroundSVG} alt="" />
+        <h1>Say What..?!</h1>
+        <div className="paragraph-container">
+          {testimonialData.content.map((testimonial) => {
+            return (
+              <div key={testimonial.id} className="recommendation">
+                <p>"{testimonial.highlight}"</p>
+                <p className="author">{testimonial.name}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
