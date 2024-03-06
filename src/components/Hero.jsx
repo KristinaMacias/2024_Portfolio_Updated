7e619625cba27f8b38762f06da232d9c2855f962
Kristina Macias <kristinamacias@Kristinas-MBP.lan>
2024-03-06 11:41:22 -0800

    added svg background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import circlesSVG from "../assets/circles-black.svg";
+import linkedinIcon from "../assets/linkedin-icon.png";
+import githubIcon from "../assets/github-icon.png";
 
 const Hero = () => {
   return (
     <section className="hero web-section">
       <div className="content-container">
         <div className="greeting-container">
+          <img className="svg-background" src={circlesSVG} alt="" />
           <h2 className="color-primary">Hi there! I'm </h2>
           <h1>Kristina Macias.</h1>
           <div className="title-container">
             <h2>
-              Full Stack Developer. <span className="color-secondary">GIS Nerd.</span>
+              Full Stack Developer.{" "}
+              <span className="color-secondary">GIS Nerd.</span>
             </h2>
           </div>
         </div>
@@ -23,8 +28,14 @@ const Hero = () => {
           </p>
         </div>
         <div className="button-container">
-          <button><a href="">LinkedIn</a></button>
-          <button><a href="">GitHub</a></button>
+          <button>
+            <img className="btn-icon" src={linkedinIcon} alt="linkedin icon" />
+            <a href="">LinkedIn</a>
+          </button>
+          <button>
+            <img className="btn-icon" src={githubIcon} alt="github icon" />
+            <a href="">GitHub</a>
+          </button>
         </div>
       </div>
     </section>
--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import testimonialData from "../data/testimonials";
+import backgroundSVG from "../assets/circles-black.svg";
 
 const Recommendations = () => {
   return (
     <section className="web-section">
       <div className="paragraph-container">
+      <img className="svg-background" src={backgroundSVG} alt="" />
       <h1>Say What..?!</h1>
         {testimonialData.content.map((testimonial) => {
           return (
